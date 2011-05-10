2c3c07c0e6fade32ac548cd825ad88b4914628a5
Simon Murtha-Smith <simon@murtha-smith.com>
2011-05-09 22:39:14 -0700

    db updates

--- db.js
+++ db.js
@@ -8,12 +8,12 @@ var dbName = 'db';
 var collectionName = 'causes';
 var server = new mongodb.Server(config.dataStoreHost, mongodb.Connection.DEFAULT_PORT, {});
 var db = new mongodb.Db(dbName, server, {native_parser:true});
-var coll;
+var causes, users;
 
 var ObjectID = db.bson_serializer.ObjectID;
 
 
-exports.openCollection = function(callback) {
+exports.openCollections = function(callback) {
     db.open(function(err, openedDB) {
         if(err) {
             console.error('error opening database', err);
@@ -23,17 +23,28 @@ exports.openCollection = function(callback) {
             db.log('could not open database', db);
             return;
         }
-        openedDB.collection(collectionName, function(err, collection) {
-            coll = collection;
-            callback(err, collection);
+        openedDB.collection('causes', function(err, collection) {
+            causes = collection;
+            openedDB.collection('users', function(err, collection) {
+                users = collection;
+                callback();
+            });
         });
     });
 }
 
 exports.putInDB = function(title, desc, hood, numPeople, dollars, callback) {
-    coll.save({title:title, desc:desc, hood:hood, numPeople:numPeople, dollars:dollars, date:new Date().getTime()}, callback);
+    causes.save({title:title, desc:desc, hood:hood, numPeople:numPeople, dollars:dollars, date:new Date().getTime()}, callback);
 }
 
+
+exports.signup = function(addr, hood, callback) {
+    users.save({email:addr, hood:hood}, callback);
+}
+
+exports.getUsersForHood = function(hood, callback) {
+    users.find({hood:hood}, callback);
+}
 
 exports.getByHood = function(hood, callback) {
     coll.find({hood:hood, date: {$gt:(new Date().getTime() - 24*60*60*1000)}}, callback);
